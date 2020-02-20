a3cc231fa01e305e23473d8cd71e486b9996ecf1
Jairo <jairobernaldev@gmail.com>
2020-02-20 14:08:28 +0100

    Tap selectable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import cytoscape from 'cytoscape';
+import { CloneVisitor } from '@angular/compiler/src/i18n/i18n_ast';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,21 +9,17 @@ import cytoscape from 'cytoscape';
 export class AppComponent implements OnInit {
   public cy: any;
   public nodeId1: String = null;
+  public nodeStyle: any = {
+    node: null,
+    backgroundStyle: null
+  };
 
   constructor() { }
 
   ngOnInit() {
     this.cy = cytoscape({
 
-      container: document.getElementById('cy'), // container to render in
-
-      // style: [
-      //   {
-      //     selector: 'node',
-      //     style: {
-            
-      //     }
-      //   },
+      container: document.getElementById('cy'),
 
       //   {
       //     selector: 'edge',
@@ -39,20 +36,29 @@ export class AppComponent implements OnInit {
         name: 'grid',
         rows: 1
       },
-      autolock:true
+      autolock: true
     });
 
     var setEdges = (node) => {
-      if(this.nodeId1 == null){
+      if (this.nodeId1 == null) {
         this.nodeId1 = node;
-      }else{
-        this.cy.add({ group: 'edges', data: { id: this.nodeId1+'_'+node, source: this.nodeId1, target: node } })
+      } else {
+        this.cy.add({ group: 'edges', data: { id: this.nodeId1 + '_' + node, source: this.nodeId1, target: node } })
         this.nodeId1 = null;
       }
     }
 
     var selectNode = (node) => {
-      this.cy.style().selector('#'+node.data.id).style({'background-color': 'black'}).update();
+      if (this.nodeStyle.node == null) {
+        this.nodeStyle.node = node.data.id;
+        this.nodeStyle.backgroundStyle = { 'background-color': node.style['background-color'].strValue }
+        this.cy.style().selector('#' + node.data.id).style({ 'background-color': 'black' }).update();
+      } else {
+        this.cy.style().selector('#' + this.nodeStyle.node).style(this.nodeStyle.backgroundStyle).update();
+        this.nodeStyle.node = null;
+        this.nodeStyle.backgroundStyle = null
+      }
+
     }
 
     //Nodos de pruebas
@@ -68,18 +74,16 @@ export class AppComponent implements OnInit {
     );
 
     //Estilo de los nodos;
-    this.cy.style().selector('#n0').style({'background-color': '#F6F'}).update();
-    this.cy.style().selector('#n1').style({'background-color': '#F6F'}).update();
-    this.cy.style().selector('#df').style({'background-color': '#F6F'}).update();
-    this.cy.style().selector('#nrf').style({'background-color': '#F65'}).update();
-    this.cy.style().selector('#nref').style({'background-color': '#F65'}).update();
-    this.cy.style().selector('#derf').style({'background-color': '#F65'}).update();
+    this.cy.style().selector('#n0').style({ 'background-color': '#F6F' }).update();
+    this.cy.style().selector('#n1').style({ 'background-color': '#F6F' }).update();
+    this.cy.style().selector('#df').style({ 'background-color': '#F6F' }).update();
+    this.cy.style().selector('#nrf').style({ 'background-color': '#F65' }).update();
+    this.cy.style().selector('#nref').style({ 'background-color': '#F65' }).update();
+    this.cy.style().selector('#derf').style({ 'background-color': '#F65' }).update();
 
-    //this.cy.remove('#n0')
     this.cy.nodes().on('click', function (e) {
       let idNode = this._private.data.id;
-      //console.log(e); //Toda la informacion con todos los nodos.
-      console.log(this._private); //Información del nodo al que se dio click.
+      //console.log(this._private); //Información del nodo al que se dio click.
       setEdges(idNode);
       selectNode(this._private);
     });
